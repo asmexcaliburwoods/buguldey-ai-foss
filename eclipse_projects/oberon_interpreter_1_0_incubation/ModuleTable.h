580938b96fa5056e92cc6b0eadcbf016865836f9
buguldey.aicommunity <buguldey.aicommunity@gmail.com>
2012-04-15 15:28:56 +0000

    tmp

--- eclipse_projects/oberon_interpreter_1_0_incubation/ModuleTable.h
+++ eclipse_projects/oberon_interpreter_1_0_incubation/ModuleTable.h
@@ -6,26 +6,61 @@ Copyright (c) 2012 Evgeniy Grigorievitch Philippov
 Distributed under the terms of GNU General Public License, v.3 or later
 */
 #include "Scanner.h"
+#include <hash_map>
+
+//for hash(wchar)
+#include <cstddef>
+
+_GLIBCXX_BEGIN_NAMESPACE(__gnu_cxx)
+
+  using std::size_t;
+
+	inline size_t
+	hash_wstring(const wchar_t* __s)
+	{
+	  unsigned long __h = 0;
+	  for ( ; *__s; ++__s)
+		__h = 5 * __h + *__s;
+	  return size_t(__h);
+	}
+
+  template<>
+    struct hash<wchar_t*>
+    {
+      size_t
+      operator()(const wchar_t* __s) const
+      { return hash_wstring(__s); }
+    };
+
+  template<>
+    struct hash<const wchar_t*>
+    {
+      size_t
+      operator()(const wchar_t* __s) const
+      { return hash_wstring(__s); }
+    };
+
+_GLIBCXX_END_NAMESPACE
+
 
 namespace Oberon {
 
 class Parser;
 class Errors;
 
-struct Module {  // object describing a declared name
-	Parser *moduleAST;
-	Module *next; // to next object in same scope //TODO reimplement as HashTable<wchar_t*,ModuleRecord*> name2moduleAST.
-};
-
 struct ModuleTable
 {
 	Errors *errors;
-	Module *topScope;
 
+private:
+	typedef __gnu_cxx::hash_map<const wchar_t*,Parser*> TModuleTable;
+	TModuleTable moduleName2parser;
+
+public:
 	ModuleTable(Errors* errors);
 	void Err(const wchar_t* msg);
-	Module* NewModule(Parser &moduleAST);
-	Parser* Find (const wchar_t* const name);
+	Parser* NewModule(Parser &moduleAST);
+	Parser* Find (const wchar_t* name);
 };
 
 }; // namespace
--- eclipse_projects/oberon_interpreter_1_0_incubation/main.cpp
+++ eclipse_projects/oberon_interpreter_1_0_incubation/main.cpp
@@ -10,27 +10,36 @@ Distributed under the terms of GNU General Public License, v.3 or later
 #include <sys/timeb.h>
 #include <wchar.h>
 #include "Interpreter.h"
+#include "checks.h"
 
 using namespace Oberon;
 
 int main (const int argc, const char *argv[]) {
-	if (argc == 2) {
+	if (argc >= 2) {
 		wchar_t *fileName = coco_string_create(argv[1]);
-		Scanner *scanner = new Scanner(fileName);
-		if(scanner==0){wprintf(L"No memory.\n");exit(1);}
-		Parser *parser = new Parser(scanner);
-		if(parser==0){wprintf(L"No memory.\n");exit(1);}
+		Scanner *scanner = new Scanner(fileName); abortIfNull(scanner);
+		Parser *parser = new Parser(scanner); abortIfNull(parser);
 		//parser->addParserListener(ParserListener)
-		ModuleTable *modules = new ModuleTable(parser->errors);
-		if(modules==0){wprintf(L"No memory.\n");exit(1);}
+		ModuleTable *modules = new ModuleTable(parser->errors); abortIfNull(modules);
 		parser->modules=modules;
-		Interpreter* interpreter = new Interpreter();
-		if(interpreter==0){wprintf(L"No memory.\n");exit(1);}
+		Interpreter* interpreter = new Interpreter();abortIfNull(interpreter);
 		parser->Parse();
 		int errorsCount=parser->errors->count;
 		if (errorsCount == 0) {
 			wprintf(L"Read success! Interpreting...\n");
-			interpreter->interpret(*parser);
+
+			const int oargc=argc-1;
+			wchar_t **oargv=(wchar_t **)malloc(sizeof(wchar_t **)*(oargc+1));abortIfNull(oargv);
+			int n=0;
+			while(n<oargc){
+				oargv[n]=coco_string_create(argv[n+1]);
+				++n;
+			}
+			oargv[n]=0;
+
+			interpreter->interpret(*parser,oargc,oargv);
+
+			n=0;while(n<oargc){coco_string_delete(oargv[n]);++n;}
 		}else{
 			wprintf(L"Read failed: %d errors.\n",errorsCount);
 		}
@@ -42,7 +51,7 @@ int main (const int argc, const char *argv[]) {
 		delete scanner;
 		if (errorsCount != 0) return 2;
 	} else {
-		wprintf(L"No file name specified or extra parameters.\n");
+		wprintf(L"No module file name specified.\n");
 		return 1;
 	}
 

8ae61bdf826328f422c83ea49e34f2a526f6af22
buguldey.aicommunity <buguldey.aicommunity@gmail.com>
2012-04-25 04:11:08 +0000

    tmp

--- eclipse_projects/descartes_example_step_tracer_1_0_incubation/src/descartes/examples/step/tracer/v1_0/infrastructure/LifeSourceInfrastructure.java
+++ eclipse_projects/descartes_example_step_tracer_1_0_incubation/src/descartes/examples/step/tracer/v1_0/infrastructure/LifeSourceInfrastructure.java
@@ -3,9 +3,10 @@ package descartes.examples.step.tracer.v1_0.infrastructure;
 import java.io.File;
 
 import descartes.examples.step.tracer.v1_0.DescartesParadigm.DescartesParadigm;
+import descartes.examples.step.tracer.v1_0.DescartesParadigm.DescartesParadigmGrammarTranslator;
 
 public class LifeSourceInfrastructure {
-	public static DescartesParadigm pickDescartesParadigm(File file) {
-		throw new UnsupportedOperationException("TODO pickDescartesParadigm");//TODO pickDescartesParadigm
+	public static DescartesParadigm parseDescartesParadigmFile(File file) {
+		return DescartesParadigmGrammarTranslator.parseParadigmFile(file);
 	}
 }

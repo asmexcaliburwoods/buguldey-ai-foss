09e7dbe8a01d3582fc2ce35d46805fdc4c3fd45f
buguldey.aicommunity@gmail.com <buguldey.aicommunity@gmail.com@3018d2c9-ab75-e0b3-50b2-bc607bdba712>
2012-04-25 03:31:51 +0000

    tmp

--- trunk/eclipse_projects/descartes_example_step_tracer_1_0_incubation/src/descartes/examples/step/tracer/v1_0/StepTracer.java
+++ trunk/eclipse_projects/descartes_example_step_tracer_1_0_incubation/src/descartes/examples/step/tracer/v1_0/StepTracer.java
@@ -1,5 +1,11 @@
 package descartes.examples.step.tracer.v1_0;
 
+import java.io.File;
+
+import descartes.examples.step.tracer.v1_0.DescartesParadigm.DescartesParadigm;
+import descartes.examples.step.tracer.v1_0.gui.StepTracerGUI;
+import descartes.examples.step.tracer.v1_0.gui.StepTracerGUIImpl;
+import descartes.examples.step.tracer.v1_0.infrastructure.LifeSourceInfrastructure;
 import descartes.examples.step.tracer.v1_0.infrastructure.UncheckedRunHelper;
 import descartes.examples.step.tracer.v1_0.infrastructure.UncheckedRunnable;
 
@@ -8,9 +14,15 @@ public class StepTracer {
 		UncheckedRunHelper.run(new UncheckedRunnable() {
 			@Override
 			public void run() throws Throwable {
-				// TODO Auto-generated method stub
-				
+				uncheckedLifeSource();				
 			}
-		};
+		});
+	}
+
+	protected void uncheckedLifeSource()throws Throwable {
+		DescartesParadigm paradigm=LifeSourceInfrastructure.pickDescartesParadigm(
+				new File("."+File.separatorChar+"paradigms"+File.separatorChar+"Descartes.descartesparadigm"));
+		StepTracerGUI gui=new StepTracerGUIImpl(paradigm);  
+		gui.stepTraceCOINTERPRET();
 	}
 }
---
+++ trunk/eclipse_projects/descartes_example_step_tracer_1_0_incubation/src/descartes/examples/step/tracer/v1_0/gui/StepTracerGUIImpl.java
@@ -0,0 +1,15 @@
+package descartes.examples.step.tracer.v1_0.gui;
+
+import descartes.examples.step.tracer.v1_0.DescartesParadigm.DescartesParadigm;
+
+public class StepTracerGUIImpl implements StepTracerGUI {
+	public StepTracerGUIImpl(DescartesParadigm paradigm) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void stepTraceCOINTERPRET() {
+		// TODO Auto-generated method stub
+		
+	}
+}

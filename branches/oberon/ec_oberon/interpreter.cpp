a479a82910615201c3366d7a1301f699dd8a58fc
buguldey.aicommunity@gmail.com <buguldey.aicommunity@gmail.com@3018d2c9-ab75-e0b3-50b2-bc607bdba712>
2013-08-16 08:09:02 +0000

    Starting to port it all to C.

---
+++ branches/oberon/ec_oberon/interpreter.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <wchar.h>
+
+#include "common.h"
+#include "SymbolTable.h"
+#include "Parser.h"
+#include "Scanner.h"
+#include <sys/timeb.h>
+#include "ModuleTable.h"
+#include "CodeGenerator.h"
+
+int run(ModTab::ModuleTable *modules, wchar_t* fileName){
+    wprintf(L"Attempting to run %ls...\n",fileName);
+	Scanner *scanner = new Scanner(fileName); abortIfNull(scanner);
+	Parser *parser = new Parser(scanner, modules->errors); abortIfNull(parser);
+	int errorsCount2=parser->errors->count;
+	parser->modtab=modules;
+	//parser->addParserListener(ParserListener)
+	parser->tab = new SymbolTable(parser); abortIfNull(parser->tab);
+	parser->gen = new CodeGenerator(modules);	abortIfNull(parser->gen);
+	wprintf(L"Parsing %ls...\n",fileName);
+	parser->Parse();
+	wprintf(L"Adding to modcache...");
+	modules->NewModule(parser, *parser->modulePtr);
+	//Obj* mo = parser->tab->NewObj("MODULE",OKscope,new TypeMODULE(),parser);
+	wprintf(L"Added.\n");
+	int errorsCount=parser->errors->count;
+	if (errorsCount == 0) {
+		wprintf(L"Parsed successfully! Interpreting %ls.\n", parser->modulePtr->moduleName);
+		errorsCount = parser->gen->InterpretModule(parser, parser->modulePtr, *(parser->tab), modules);
+	}else{
+		wprintf(L"Parsing of %ls failed: %d errors.\n",fileName, errorsCount);
+	}
+
+/*	coco_string_delete(fileName);
+	delete parser->gen;
+	delete parser->tab;
+	delete parser;
+	delete scanner;
+*/
+	return errorsCount;
+}
+
